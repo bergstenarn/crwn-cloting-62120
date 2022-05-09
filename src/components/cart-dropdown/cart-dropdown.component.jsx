b0b7ccfea54215e378e258180a2e846f642b3cfd
bergstenarn <andersbergsten@yahoo.se>
2022-05-09 21:11:20 +0300

    137. Styled-Component - Cart Dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,8 +3,11 @@ import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
-import { CartDropdownContainer, CartItems } from "./cart-dropdown.styles.jsx";
-import React from "react";
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const { cartItems, toggleCartOpen } = useContext(CartContext);
@@ -18,9 +21,11 @@ const CartDropdown = () => {
   return (
     <CartDropdownContainer>
       <CartItems>
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
       </CartItems>
       <Button style={{ "margin-top": "auto" }} onClick={goToCheckoutHandler}>
         CHECKOUT
