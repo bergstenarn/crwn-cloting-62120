affaf54a5dfc2cfe10e7f7c83b8f96e65f64e020
bergstenarn <andersbergsten@yahoo.se>
2022-05-28 22:19:37 +0300

    168. Asynchronous Redux: Redux-Thunk

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 // import logger from "redux-logger";
 import logger from "./middleware/logger";
+import thunk from "redux-thunk";
 import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
@@ -13,9 +14,10 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
