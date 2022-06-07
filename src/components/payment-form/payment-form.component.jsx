882ffa7e9dfa24ee1916a7a116453a9513e90fd3
bergstenarn <andersbergsten@yahoo.se>
2022-06-07 21:16:03 +0300

    185. Finishing Stripe Payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -6,11 +6,46 @@ const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
 
-  if (!stripe || !elements) return <h4>Payment currently unavailable</h4>;
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) return <h4>Payment currently unavailable</h4>;
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Anders Bergsten",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful");
+      }
+    }
+  };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment</h2>
         <CardElement />
         <br />
